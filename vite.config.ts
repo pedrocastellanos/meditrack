ff33bbc0d3ddf0ad74f9bd11f0b12c09995886f5
Pedro Castellanos Alonso <pedro.castellanos2003@gmail.com>
2026-05-23 09:39:14 -0400

    Separar configuración de Vitest a archivo independiente

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
@@ -10,8 +9,4 @@ export default defineConfig({
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss()
   ],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-  },
 })
